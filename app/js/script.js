0a8a798eb5918d22f4ca728a67a1edfefdefe66e
daniellmid <103086154+daniellmid@users.noreply.github.com>
2022-05-02 21:19:20 -0500

    navbar Menu effect
    navbar Menu effect (smooth) was added.
    
    other minor changes.

--- app/js/script.js
+++ app/js/script.js
@@ -1,53 +1,51 @@
-//--------------------------------------------Navbar visibility 
+//--------------------------------------------Navbar visibility
 const navbar = document.querySelector(".navbar-container");
 const section = document.getElementById("header");
 
 const activationPoint = {
-  rootMargin: "-50px 0px 0px 0px"
+  rootMargin: "-50px 0px 0px 0px",
 };
-const headerSectionObserver = new IntersectionObserver(
-    function (e, headerSectionObserver) {
-        e.forEach(entry => {
-            if (!entry.isIntersecting) {
-                navbar.style.top="0";
-                navbarList.style.transition=".4s ease-in-out";
-                console.log(entry);
-            } else {
-                navbarList.style.transition="0s"
-                navbarList.classList.remove("navbar-list-active");
-                navbarBtn.classList.remove("toggle-nb");               
-                navbarLinks.forEach((link, i)=>{
-                link.style.animation="";
-            });
-            navbar.style.top="-5.5rem";
-            }
-        });
-    },
+const headerSectionObserver = new IntersectionObserver(function (
+  e,
+  headerSectionObserver
+) {
+  e.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      navbar.style.top = "0";
+      navbarList.style.transition = ".4s ease-in-out";
+      console.log(entry);
+    } else {
+      navbarList.classList.remove("navbar-list-active");
+      navbarBtn.classList.remove("toggle-nb");
+      navbarLinks.forEach((link, i) => {
+        link.style.animation = "";
+      });
+      navbar.style.top = "-5.5rem";
+    }
+  });
+},
 activationPoint);
 
 headerSectionObserver.observe(section);
 
-
-
-
 //--------------------------------------------Scroll movement effect
-const dataTarget = document.querySelectorAll('[data-target]');
-const dataContent = document.querySelectorAll('[data-content]');
+const dataTarget = document.querySelectorAll("[data-target]");
+const dataContent = document.querySelectorAll("[data-content]");
 //------------Open tabs aside
 const tabSideBtn = document.querySelector(".tab-side-btn");
 
 dataTarget.forEach((link) => {
   link.addEventListener("click", () => {
     const select = document.querySelector(link.dataset.target);
-    
+
     if (select.classList.contains("tab-side")) {
-        select.classList.add("tab-side-active");
-        tabSideBtn.classList.add("tsb-active");
-        tabSideBtn.addEventListener("click", ()=>{
-            select.classList.remove("tab-side-active");
-            tabSideBtn.classList.remove("tsb-active");
-        })
-    } else {      
+      select.classList.add("tab-side-active");
+      tabSideBtn.classList.add("tsb-active");
+      tabSideBtn.addEventListener("click", () => {
+        select.classList.remove("tab-side-active");
+        tabSideBtn.classList.remove("tsb-active");
+      });
+    } else {
       select.scrollIntoView({
         behavior: "smooth",
       });
@@ -55,20 +53,27 @@ dataTarget.forEach((link) => {
   });
 });
 
-
-
-
 //--------------------------------------------Navbar Menu effect
 const navbarBtn = document.querySelector(".navbar-buttom");
 const navbarList = document.querySelector(".navbar-list");
-const navbarLinks = document.querySelectorAll(".navbar-link"); 
-navbarBtn.addEventListener("click", ()=>{
-    //toggle navbar    
-    navbarList.classList.toggle("navbar-list-active");    
-    navbarBtn.classList.toggle("toggle-nb");
-    //animation links
-    navbarLinks.forEach((link, i)=>{
-        link.style.animation? link.style.animation="" : link.style.animation=`linksFade 0.4s ease forwards ${i / 15 + 0.1}s`;
-    })
-})
-
+const navbarLinks = document.querySelectorAll(".navbar-link");
+navbarBtn.addEventListener("click", () => {
+  //toggle navbar
+  navbarList.classList.toggle("navbar-list-active");
+  navbarList.classList.contains("navbar-list-active")
+    ? (navbarList.style.animation = "navbarBoxOut 0.4s ease-in-out")
+    : (navbarList.style.animation =
+        "navbarBoxIn 0.4s ease-in-out .4s backwards");
+  navbarBtn.classList.toggle("toggle-nb");
+  //animation links
+  navbarLinks.forEach((link, i) => {
+    //link.style.animation? link.style.animation="" : link.style.animation=`linksFade 0.4s ease forwards ${i / 15 + 0.1}s`;
+    navbarList.classList.contains("navbar-list-active")
+      ? (link.style.animation = `linksFadeOut 0.4s ease forwards ${
+          i / 15 + 0.1
+        }s`)
+      : (link.style.animation = `linksFadeIn 0.4s ease backwards ${
+          i / 15 + 0.1
+        }s`);
+  });
+});
