affeaffd765968090800bcf9778b3b2d958caf42
daniellmid <103086154+daniellmid@users.noreply.github.com>
2022-05-06 12:35:26 -0500

    Mobile Screen size, Tabs aside and orchid menu changes
    Mobile Screen size: using variables to pass screen sizes.
    
    Tabs aside: sync timing of entry and fixing navbar effect scroll up.
    
    Orchid menu: reduce upper left/right elements to avoid force website gets wider than the original width.
    
    "disabled" touch highlights (color transparent)

--- app/js/script.js
+++ app/js/script.js
@@ -1,9 +1,8 @@
 //--------------------------------------------Screen Size adjustment (mobile)
 const documentHeight = () => {
   const doc= document.documentElement;
-  var w = doc.clientWidth;
-  var h = doc.clientHeight;
-  
+    var w = doc.clientWidth;
+    var h = doc.clientHeight;
   doc.style.setProperty("--doc-width", `${w}px`);
   doc.style.setProperty("--doc-height", `${h}px`);
   console.log(`Width: ${w}px , height: ${h}px`);
@@ -22,41 +21,44 @@ const activationPoint = {
 const headerSectionObserver = new IntersectionObserver(function (e, headerSectionObserver) {
   e.forEach((entry) => {
     if (!entry.isIntersecting) {
-      navbar.style.top = "4.7rem";
-      console.log("funciono, salio");
+      navbarTranslateIn();
       navbarList.style.transition = ".4s ease-in-out";
-      console.log(entry);
+      //console.log(entry);
     } else {
       navbarList.classList.remove("navbar-list-active");
       navbarBtn.classList.remove("toggle-nb");
       navbarLinks.forEach((link, i) => {
         link.style.animation = "";
       });
-      navbar.style.top = "-4.7rem";
-      console.log("funciono, se oculto")
+      navbarTranslateOut();
     }
   });
 }, activationPoint);
 
 headerSectionObserver.observe(section);
 
+function navbarTranslateIn(){
+  navbar.style.transform = "translateY(0rem)";
+}
+
+function navbarTranslateOut(){
+  navbar.style.transform = "translateY(-4.7rem)";
+}
+
+
+
 //--------------------------------------------Scroll movement effect
 const dataTarget = document.querySelectorAll("[data-target]");
 const dataContent = document.querySelectorAll("[data-content]");
-//------------Open tabs aside
-const tabSideBtn = document.querySelector(".tab-side-btn");
-
 dataTarget.forEach((link) => {
   link.addEventListener("click", () => {
     const select = document.querySelector(link.dataset.target);
 
     if (select.classList.contains("tab-side")) {
-      select.classList.add("tab-side-active");
-      tabSideBtn.classList.add("tsb-active");
-      tabSideBtn.addEventListener("click", () => {
-        select.classList.remove("tab-side-active");
-        tabSideBtn.classList.remove("tsb-active");
-      });
+      if(navbarBtn.classList.contains("toggle-nb")){
+        navbarEffect();
+      }
+      setTimeout(function(){tabSide(select)}, 500);         
     } else {
       select.scrollIntoView({
         behavior: "smooth",
@@ -64,28 +66,49 @@ dataTarget.forEach((link) => {
     }
   });
 });
+//------------Open tabs aside in
+function tabSide(select){
+  const tabSideBtn = document.querySelector(".tab-side-btn");
+  select.classList.add("tab-side-active");
+  tabSideBtn.classList.add("tsb-active");
+  tabSideBtn.addEventListener("click", () => {
+    select.classList.remove("tab-side-active");
+    tabSideBtn.classList.remove("tsb-active");
+    clearTimeout(tabSide);       
+  });
+}
+
+
 
 //--------------------------------------------Navbar Menu effect
 const navbarBtn = document.querySelector(".navbar-buttom");
 const navbarList = document.querySelector(".navbar-list");
-const navbarLinks = document.querySelectorAll(".navbar-link");
+const navbarLinks = document.querySelectorAll(".navbar-link");  
 navbarBtn.addEventListener("click", () => {
-  //toggle navbar
-  navbarList.classList.toggle("navbar-list-active");
-  navbarList.classList.contains("navbar-list-active")
-    ? (navbarList.style.animation = "navbarBoxOut 0.4s ease-in-out")
-    : (navbarList.style.animation =
-        "navbarBoxIn 0.4s ease-in-out .4s backwards");
-  navbarBtn.classList.toggle("toggle-nb");
-  //animation links
-  navbarLinks.forEach((link, i) => {
-    //link.style.animation? link.style.animation="" : link.style.animation=`linksFade 0.4s ease forwards ${i / 15 + 0.1}s`;
-    navbarList.classList.contains("navbar-list-active")
-      ? (link.style.animation = `linksFadeOut 0.4s ease forwards ${
-          i / 15 + 0.1
-        }s`)
-      : (link.style.animation = `linksFadeIn 0.4s ease backwards ${
-          i / 15 + 0.1
-        }s`);
-  });
+  navbarEffect(); 
 });
+
+//toggle navbar
+function navbarEffect(){
+  navbarList.classList.toggle("navbar-list-active");   
+  navbarBtn.classList.toggle("toggle-nb");
+  if(navbarList.classList.contains("navbar-list-active")){
+    navbarList.style.visibility = "visible";
+    navbarList.style.animation = "navbarBoxOut 0.4s ease-in-out";    
+  } else{     
+      navbarList.style.animation = "navbarBoxIn 0.4s ease-in-out .4s backwards";
+      setTimeout(visibility, 700);
+    }
+  navbarLinks.forEach((link, i) => {
+    if(navbarList.classList.contains("navbar-list-active")){
+      link.style.animation = `linksFadeIn 0.4s ease forwards ${i / 15 + 0.1}s`;
+    } else{ 
+        link.style.animation = `linksFadeOut 0.4s ease backwards ${i / 15 + 0.1}s`;
+      }
+  });
+}
+
+function visibility(){
+  navbarList.style.visibility = "hidden";  
+  clearTimeout(visibility); 
+}
